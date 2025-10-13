b7144b2f59866eb2558c7cde52d746f380342708
abdelrahman-ayman-77 <abdelrahman.ayman.sabry@gmail.com>
2025-10-13 15:47:17 +0300

    initial commit

---
+++ lib/screens/splashscreen.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _curvedAnimation;
+
+  late final AnimationController _circleController;
+  late final Animation<double> _circleSizeAnimation;
+
+  late final AnimationController _moveController;
+  late final Animation<double> _moveAnimation;
+
+  late final AnimationController _textController;
+  late final Animation<double> _textOpacity;
+  late final Animation<double> _textSlide;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // First animation (shader slide)
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+
+    _curvedAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOutCubic,
+    );
+
+    // Second animation (circle shrink)
+    _circleController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+
+    _circleSizeAnimation = Tween<double>(begin: 150, end: 90).animate(
+      CurvedAnimation(parent: _circleController, curve: Curves.easeOutCubic),
+    );
+
+    // Third animation (move left)
+    _moveController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+
+    _moveAnimation = Tween<double>(begin: 0, end: -100).animate(
+      CurvedAnimation(parent: _moveController, curve: Curves.easeInOut),
+    );
+    // 4️⃣ Text appear (fade + slide)
+    _textController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+    _textOpacity = CurvedAnimation(
+      parent: _textController,
+      curve: Curves.easeIn,
+    );
+    _textSlide = Tween<double>(
+      begin: 30,
+      end: 0,
+    ).animate(CurvedAnimation(parent: _textController, curve: Curves.easeOut));
+    // Start first animation
+    _controller.forward();
+
+    // After shader finishes → shrink circle
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _circleController.forward();
+      }
+    });
+
+    // After circle shrink finishes → move left
+    _circleController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _moveController.forward();
+      }
+    });
+
+    _moveController.addStatusListener((status) {
+      if (status == AnimationStatus.forward) {
+        // Start showing text as the circle moves
+        _textController.forward();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _circleController.dispose();
+    _moveController.dispose();
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color(0xFF215DF7), Color(0xFF0C1F51)],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: Center(
+        child: AnimatedBuilder(
+          animation: Listenable.merge([
+            _circleSizeAnimation,
+            _curvedAnimation,
+            _moveAnimation,
+            _textOpacity,
+            _textSlide,
+          ]),
+          builder: (context, child) {
+            return Transform.translate(
+              offset: Offset(_moveAnimation.value, 0), // 👈 move left
+              child: Container(
+                width: _circleSizeAnimation.value,
+                height: _circleSizeAnimation.value,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: Colors.white,
+                ),
+                child: ShaderMask(
+                  shaderCallback: (bounds) {
+                    return LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      stops: [
+                        _curvedAnimation.value,
+                        (_curvedAnimation.value + 0.1).clamp(0.0, 1.0),
+                      ],
+                      colors: [Colors.black, Colors.transparent],
+                    ).createShader(bounds);
+                  },
+                  blendMode: BlendMode.dstIn,
+                  child: Image.asset('assets/images/Vector.png'),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
